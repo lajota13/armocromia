ed51f5af45bf05189b328910eff76a6769d42c82
giacomo <giacobub@gmail.com>
2022-01-05 19:12:56 +0100

    added exception logging

--- scripts/preprocess_dataset.py
+++ scripts/preprocess_dataset.py
@@ -1,3 +1,4 @@
+import logging
 import glob
 import os
 import argparse
@@ -13,12 +14,20 @@ from tqdm import tqdm
 WORKERS_NUM = 8
 
 
+def init_logger(path: str):
+    logger = logging.getLogger(__name__)
+    fh = logging.FileHandler(path)
+    logger.addHandler(fh)
+    return logger
+
+
 class Cropper(Thread):
-    def __init__(self, paths: List[str], dst_root: str, pbar: tqdm):
+    def __init__(self, paths: List[str], dst_root: str, pbar: tqdm, logger: logging.Logger):
         self.paths = paths
         self.dst_root = dst_root
         self.detector = MTCNN()
         self.pbar = pbar
+        self.logger = logger
         super().__init__()
 
     def run(self):
@@ -26,18 +35,22 @@ class Cropper(Thread):
             self.preprocess(p)
 
     def preprocess(self, path: str):
-        img = Image.open(path)
-        faces = self.detector.detect_faces(np.array(img))
-        if len(faces) == 1:
-            x, y, w, h = faces[0]["box"]
-            img_crop = img.crop((x, y, x + w, y + h))
-            img.close()
-            dst_path = "/".join([self.dst_root] + path.split("/")[-2:])
-            img_crop.save(dst_path)
-            img_crop.close()
-        else:
-            img.close()
-        self.pbar.update(1)
+        try:
+            img = Image.open(path)
+            faces = self.detector.detect_faces(np.array(img))
+            if len(faces) == 1:
+                x, y, w, h = faces[0]["box"]
+                img_crop = img.crop((x, y, x + w, y + h))
+                img.close()
+                dst_path = "/".join([self.dst_root] + path.split("/")[-2:])
+                img_crop.save(dst_path)
+                img_crop.close()
+            else:
+                img.close()
+            self.pbar.update(1)
+        except:
+            self.logger.exception(f"Could not preprocess {path}")
+            self.pbar.update(1)
 
 
 def split_paths(paths: List[str], n: int) -> List[List[str]]:
@@ -49,11 +62,11 @@ def split_paths(paths: List[str], n: int) -> List[List[str]]:
     return chunks
 
 
-def preprocess_season(season_dir: str, dst_root: str):
+def preprocess_season(season_dir: str, dst_root: str, logger):
     paths = glob.glob(season_dir + "/*.png")
     chunks = split_paths(paths, WORKERS_NUM)
     with tqdm(total=len(paths)) as pbar:
-        croppers = [Cropper(c, dst_root, pbar) for c in chunks]
+        croppers = [Cropper(c, dst_root, pbar, logger) for c in chunks]
         for c in croppers:
             c.start()
         for c in croppers:
@@ -63,11 +76,12 @@ def preprocess_season(season_dir: str, dst_root: str):
 def preprocess_dataset(src_root: str):
     dst_root = f"{src_root}_preprocessed"
     os.mkdir(dst_root)
+    logger = init_logger(dst_root + "/exceptions.log")
     seasons_dirs = glob.glob(src_root + "/*/")
     for i, season_dir in enumerate(seasons_dirs, 1):
         os.mkdir(dst_root + "/" + season_dir.split("/")[-2])
         print(f"{season_dir.split('/')[-1]} ({i}/{len(seasons_dirs)})")
-        preprocess_season(season_dir, dst_root)
+        preprocess_season(season_dir, dst_root, logger)
 
 
 if __name__ == "__main__":
